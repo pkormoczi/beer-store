1cdf890f99650621b68393b5dfe89b6163fee4f3
KHB\u94732 <peter.kormoczi@kh.hu>
2018-08-10 18:14:02 +0200

    web security config

--- beer-store-application/src/main/java/dev/ronin/demo/beerstore/endpoint/configruation/WebSecurityConfig.java
+++ beer-store-application/src/main/java/dev/ronin/demo/beerstore/endpoint/configruation/WebSecurityConfig.java
@@ -1,4 +1,27 @@
 package dev.ronin.demo.beerstore.endpoint.configruation;
 
-public class WebSecurityconfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/services/**").hasRole("USER")
+            .and().httpBasic()
+            .and().csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
+            .withUser("user").password("{noop}password").roles("USER");
+    }
 }
