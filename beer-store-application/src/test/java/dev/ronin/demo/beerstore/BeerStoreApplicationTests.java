a60c98c67d3aa1cda902b1a152f42997f46c3119
pkormoczi <theronin86@protonmail.com>
2018-09-15 00:57:27 +0200

    junit 5 -> 4

--- beer-store-application/src/test/java/dev/ronin/demo/beerstore/BeerStoreApplicationTests.java
+++ beer-store-application/src/test/java/dev/ronin/demo/beerstore/BeerStoreApplicationTests.java
@@ -1,17 +1,17 @@
 package dev.ronin.demo.beerstore;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BeerStoreApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
--- beer-store-domain/src/main/test/java/dev/ronin/demo/beerstore/domain/order/OrderServiceTest.java
+++ beer-store-domain/src/main/test/java/dev/ronin/demo/beerstore/domain/order/OrderServiceTest.java
@@ -1,16 +1,16 @@
 package dev.ronin.demo.beerstore.domain.order;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class OrderServiceTest {
 
     @Mock
