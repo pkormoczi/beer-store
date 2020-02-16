ef0f83b441872756278e6854e36020c33d4e51d5
pkormoczi <theronin86@protonmail.com>
2020-02-16 21:08:05 +0100

    Convert to YAML format Change TestMode to WebTestClient

--- beer-store-application/src/test/java/dev/ronin/demo/beerstore/contract/ContractTestBase.java
+++ beer-store-application/src/test/java/dev/ronin/demo/beerstore/contract/ContractTestBase.java
@@ -1,36 +1,32 @@
 package dev.ronin.demo.beerstore.contract;
 
-import dev.ronin.demo.beerstore.BeerStoreApplication;
 import dev.ronin.demo.beerstore.contract.customer.Address;
 import dev.ronin.demo.beerstore.contract.customer.Customer;
 import dev.ronin.demo.beerstore.infrastructure.adapter.CustomerAdapter;
 import dev.ronin.demo.beerstore.infrastructure.controller.CustomerController;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import io.restassured.module.mockmvc.config.MockMvcConfig;
+import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.context.WebApplicationContext;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = BeerStoreApplication.class)
+//@SpringBootTest(classes = BeerStoreApplication.class)
+@ExtendWith(MockitoExtension.class)
 public class ContractTestBase {
 
-    @Autowired
-    private WebApplicationContext context;
+//    @Autowired
+//    private CustomerController customerController = new C;
 
-    @Autowired
-    private CustomerController customerController;
-
-    @MockBean
+    @Mock
     private CustomerAdapter customerAdapter;
 
     @BeforeEach
     void beforeEach() {
         final Customer customer = new Customer(1, "TestFirst", "TestLast", new Address("MockCountry", "MockCity", "MockAddress", "1111"));
         Mockito.when(this.customerAdapter.findCustomer("TestFirst")).thenReturn(customer);
-        RestAssuredMockMvc.standaloneSetup(this.customerController);
+//        RestAssuredMockMvc.standaloneSetup(this.customerController);
+        RestAssuredWebTestClient.standaloneSetup(new CustomerController(this.customerAdapter));
     }
 
 }
