04ef37de4a65ab543aa1eb8b6616d9bff994a6ec
Körmöczi Péter <peter.kormoczi@kh.hu>
2025-05-13 01:13:05 +0200

    Update tests

--- beer-store-client/src/test/java/dev/ronin/demo/client/CustomerControllerTest.java
+++ beer-store-client/src/test/java/dev/ronin/demo/client/CustomerControllerTest.java
@@ -1,7 +1,8 @@
 package dev.ronin.demo.client;
 
+import dev.ronin.demo.beerstore.client.infrastructure.api.model.AddressModel;
 import dev.ronin.demo.beerstore.client.infrastructure.api.model.CustomerModel;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
@@ -9,8 +10,11 @@ import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.NONE)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureStubRunner(
         stubsMode = StubRunnerProperties.StubsMode.CLASSPATH,
         ids = "dev.ronin.demo:beer-store-contract:+:stubs",
@@ -20,21 +24,54 @@ class CustomerControllerTest {
     @StubRunnerPort("beer-store-contract")
     int port;
 
+    private static final String BASE_URL_FORMAT = "http://localhost:%d";
+
+
+    private WebTestClient testClient;
+
+    @BeforeEach
+    void setUp() {
+        testClient = WebTestClient
+                .bindToServer()
+                .baseUrl(String.format(BASE_URL_FORMAT, port))
+                .build();
+    }
+
+
 
     @Test
-    void testCustomerContract() {
-
-        WebTestClient testClient = WebTestClient
-                .bindToServer()
-                .baseUrl("http://localhost:" + this.port)
-                .build();
+    void testGetExistingCustomer() {
         final CustomerModel responseBody = testClient.get().uri("/customers/search?name=TestFirst")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus()
                 .isOk()
                 .expectBody(CustomerModel.class).returnResult().getResponseBody();
-        Assertions.assertThat(responseBody.getFirstName()).isEqualTo("TestFirst");
 
+        assertThat(responseBody.getFirstName()).isEqualTo("TestFirst");
+    }
+
+    @Test
+    void testCreateCustomer() {
+        final CustomerModel responseBody = testClient.post().uri("/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(newCustomer()), CustomerModel.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(CustomerModel.class).returnResult().getResponseBody();
+
+        assertThat(responseBody.getId()).isNotNull();
+    }
+
+    private CustomerModel newCustomer() {
+        CustomerModel customer = new CustomerModel();
+        customer.setFirstName("TestFirst");
+        customer.setLastName("TestLast");
+        customer.setAddress(new AddressModel());
+        customer.getAddress().setCountry("MockCountry");
+        customer.getAddress().setCity("MockCity");
+        customer.getAddress().setStreetAddress("MockAddress");
+        customer.getAddress().setZip("1111");
+        return customer;
     }
 }
